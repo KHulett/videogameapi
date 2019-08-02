7d49dcd3af7dd88c8540a4dfc79f811baf95e696
K Hulett <kblocent@gmail.com>
2019-08-02 12:36:37 -0400

    trying igdb api node

--- src/App.js
+++ src/App.js
@@ -1,26 +1,79 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import axios from "axios";
+import igdb from 'igdb-api-node';
+import PlatformSelector from './components/PlatformSelector';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// const client = igdb(process.env.REACT_APP_API_KEY)
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      gameslist: null
+    }
+  }
+
+// async componentDidMount(){
+//   axios({
+//     // url: "https://api-v3.igdb.com/games?limit=25",
+//     url: 'https://cors-anywhere.herokuapp.com/https://api-v3.igdb.com/platforms/?search=playstation&fields=id,name,generation',
+//     method: 'GET',
+//     headers: {
+//         'Accept': 'application/json',
+//         'user-key': client
+//     },
+//     data: "fields abbreviation,alternative_name,category,created_at,generation,name,platform_logo,product_family,slug,summary,updated_at,url,versions,websites;"
+//   })
+//   .then(response => this.setState({games: response}))
+
+//     .then(response => console.log(response))
+//     .catch(err => {
+//         console.error(err);
+//     }) 
+//   }
+
+async componentDidMount(){
+  const client = igdb(process.env.REACT_APP_API_KEY)
+const response = await client.games({
+  
+  filters: {
+      'release_dates.date-gt': '2010-12-31',
+      'release_dates.date-lt': '2012-01-01'
+  },
+  limit: 5,
+  offset: 0,
+  order: 'release_dates.date:desc',
+  search: 'zelda'
+}, [
+  'name',
+  'release_dates.date',
+  'rating',
+  'hypes',
+  'cover'
+])
+// .then(response => this.setState({gameslist: response}))
+// .then(response => console.log(response))
+    .catch(err => {
+        console.error(err);
+})
 }
 
+  render() { 
+
+    if(!this.state.games){
+      return <div>loading...</div>
+    } else {
+    return (
+      <div>
+
+        <ul> 
+       {this.state.gameslist.data.map((names) => <li key={names.name}>{names.name}{names.release_date.date}</li>)}
+        </ul>
+      </div> 
+    
+    )
+    }
+  }
+}
+ 
 export default App;
---
+++ src/components/PlatformSelector.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+let PlatformSelector = [
+    {
+        question: "what platform do you want to play on?",
+        answers: [
+            {
+                type:"Playstation",
+                content: "Playstation 4"
+            },
+
+            {
+                type:"Xbox",
+                content: "Xbox One"
+            },
+
+            {
+                type:"Nintendo",
+                content:"Nintendo Switch"
+            }
+        ]
+    },
+]
+
+export default PlatformSelector;
